72786a140e4ce8cc9ce935bb080edd286229d5c3
Shokhrukh Karimov <shokhrukhkarimov@192.168.1.5>
2025-01-27 16:42:17 +0500

    feat: blog site deployment

--- next.config.js
+++ next.config.js
@@ -4,20 +4,20 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       env: {
-        mongodb_username: "shokhrukh1212",
-        mongodb_password: "eXXB2DIMgmy6l3Cn",
-        mongodb_clustername: "cluster0",
-        mongodb_database: "my-site-dev",
+        mongodb_username: process.env.MONGODB_USERNAME,
+        mongodb_password: process.env.MONGODB_PASSWORD,
+        mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
+        mongodb_database: process.env.MONGODB_DATABASE_DEV,
       },
     };
   }
 
   return {
     env: {
-      mongodb_username: "shokhrukh1212",
-      mongodb_password: "eXXB2DIMgmy6l3Cn",
-      mongodb_clustername: "cluster0",
-      mongodb_database: "my-site",
+      mongodb_username: process.env.MONGODB_USERNAME,
+      mongodb_password: process.env.MONGODB_PASSWORD,
+      mongodb_clustername: process.env.MONGODB_CLUSTERNAME,
+      mongodb_database: process.env.MONGODB_DATABASE_PROD,
     },
   };
 };
